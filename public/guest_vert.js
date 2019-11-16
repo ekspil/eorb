f6b5bd0cbae71d0f3b7cff2b5fa491704fd6dd81
ekspil <ekspil@yandex.ru>
2019-11-16 12:15:09 +1000

    version 3 realise, 3.3.6 try to find spamer

--- public/guest_vert.js
+++ public/guest_vert.js
@@ -454,21 +454,21 @@ if (!manager){
   $('#client-box').append(`
 
 
-    <div class="uk-width-2-10 " >  <!--   "uk-width-1-5 uk-hidden-small uk-hidden-mini"      -->
+    <div class="uk-width-3-10 " >  <!--   "uk-width-1-5 uk-hidden-small uk-hidden-mini"      -->
         <div class="uk-panel uk-panel-space"><div class="uk-panel-teaser notready-img">
     <img onclick="playSound()" class ="uk-align-center" src="/data/notready_vert.png" alt="">
     </div></div><br>
 <div class="uk-grid uk-flex uk-flex-wrap" id="notready-list"></div>
 
         </div>
-    <div class="uk-width-2-10 " >
+    <div class="uk-width-3-10 " >
       <div class="uk-panel uk-panel-space "><div class="uk-panel-teaser ready-img " id="panelTeaser">
     <img class ="uk-align-center " src="/data/ready_vert.png" alt="">
     </div></div><br>
 <div class="uk-grid" id="ready-list"></div>
     </div>
-    <div class="uk-width-6-10 uk-cover-container"  id="bg-img">
-
+    <div class="uk-width-4-10 uk-cover-container"  id="bg-img">
+    <br><br><br><br><br><br><br><br><br><br><br>
     <video autoplay loop muted playsinline uk-cover>
         <source src="/video/1234.mp4" type="video/mp4" autostart="true">
 
